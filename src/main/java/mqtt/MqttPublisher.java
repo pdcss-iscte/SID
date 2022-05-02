a11580e92313889088f0a7563f28461b490e6b89
Rita Regada <rpraa@iscte-iul.pt>
2022-05-02 19:19:24 +0100

    wip

--- src/main/java/mqtt/MqttPublisher.java
+++ src/main/java/mqtt/MqttPublisher.java
@@ -1,5 +1,6 @@
 package mqtt;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.Random;
 import java.util.Timer;
@@ -9,6 +10,7 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.util.JSON;
+import logic.IniReader;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
@@ -18,24 +20,42 @@ import org.json.JSONObject;
 
 public class MqttPublisher {
 
-	public static void main(String[] args) throws MqttException, InterruptedException {
+	public static IMqttClient mqttConnector(){
+		try {
+		System.out.println("Connecting to MQTT...");
 		String cloudServer = "tcp://broker.mqtt-dashboard.com:1883";
 		String cloudTopic = "sid2022_g05";
-		
 		String clientId = "sid2022_g05";
-		IMqttClient mqttClient = new MqttClient(cloudServer,clientId);
-		
+		IMqttClient mqttClient = null;
+		mqttClient = new MqttClient(cloudServer,clientId);
 		MqttConnectOptions options = new MqttConnectOptions();
 		options.setAutomaticReconnect(true);
 		options.setCleanSession(true);
 		options.setConnectionTimeout(10);
 		mqttClient.connect(options);
+		System.out.println("Connected!");
+		return mqttClient;
+		} catch (MqttException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
+	public static DBCursor dbConnector(){
+	try {
+		IniReader localDB =  IniReader.getLocalDatabase();
 		DBCollection tempCol = localDB.getCollection("temp");
 		DBCursor cursor = tempCol.find();
 		DBCollection measurementsCol = localDB.getCollection("measurement");
+		return cursor;
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
 
-		long elapsedTime=0;
+	public static void publish(){
+		IMqttClient mqttClient = mqttConnector();
+		DBCursor cursor = dbConnector();
+		System.out.println("Sending data...");
 		while (cursor.hasNext()){
 			long start = System.nanoTime();
 			DBObject temp = cursor.next();
@@ -45,11 +65,13 @@ public class MqttPublisher {
 			msg.setQos(0);
 			msg.setRetained(false);
 			mqttClient.publish(cloudTopic,msg);
-			elapsedTime= System.nanoTime() - start;
-
+			System.out.println("Data sended succesfully");
 		}
-		Thread.sleep(2000-elapsedTime);
+		Thread.sleep(2000);
 
+	}
+	public static void main(String[] args) throws MqttException, InterruptedException {
+	publish();
 
 	}
 
